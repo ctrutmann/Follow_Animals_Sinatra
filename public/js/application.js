02dfc5fdce58aeaadd7b3a76addfc4f706ab0af3
Christel Trutmann <ctrutmann@gmail.com>
2016-06-30 16:20:07 -0700

    Add jQuery and CSS to animal divs linking to individual animals/show

--- public/js/application.js
+++ public/js/application.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $('.title').on('click', function() {
     window.location.href='/';
   });
+
+  $('.animal').on('click', function() {
+    id = $(this).attr('id')
+    window.location.href='/animals/'+ id;
+  });
 });
