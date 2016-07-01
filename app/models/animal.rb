078afdb276d15fe9ec44976a68eaf1633f3c6c42
Christel Trutmann <ctrutmann@gmail.com>
2016-06-30 18:30:47 -0700

    Add animals/show

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,6 +4,16 @@ class Animal < ActiveRecord::Base
   has_many :follower_animals
   has_many :followers, :through => :follower_animals, :source => :user
 
+  before_validation :match_status_and_needs_help
+
   validates :name, :species, :zip_code, :guardian_id, presence: true
+
+  def match_status_and_needs_help
+    if self.status_id == 1 || self.status_id == 5
+      self.needs_help = false
+    else
+      self.needs_help = true
+    end
+  end
 
 end
